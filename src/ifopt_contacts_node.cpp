992553878eef5a51c32f8b7f38888b4d65a27ec2
matteoparigi <matteo.parigi@iit.it>
2019-05-13 11:50:29 +0200

    pushing phase

--- src/ifopt_contacts_node.cpp
+++ src/ifopt_contacts_node.cpp
@@ -18,7 +18,7 @@ using namespace ifopt;
 XBot::MatLogger::Ptr logger;
 std::map<std::string, Eigen::Vector6d> * g_fmap_ptr;
 
-void set_low_stiffness(XBot::RobotInterface::Ptr robot)
+void set_legs_low_stiffness(XBot::RobotInterface::Ptr robot)
 {
     
     return;
@@ -46,6 +46,74 @@ void set_low_stiffness(XBot::RobotInterface::Ptr robot)
             Eigen::VectorXd leg_k(robot->leg(i).getJointNum());
             leg_k = K_0 + k/(N_ITER-1)*(K_end-K_0);
             robot->leg(i).setStiffness(leg_k);
+        }
+        
+        robot->move();
+        
+        ros::Duration(0.01).sleep();
+    }
+   
+}
+
+void set_arms_low_stiffness(XBot::RobotInterface::Ptr robot, Eigen::VectorXd& K_0)
+{
+          
+    robot->setControlMode(XBot::ControlMode::Stiffness() + XBot::ControlMode::Damping());
+    
+    // dont send any impedance to the wheels
+    std::map<std::string, XBot::ControlMode> ctrl_map;
+    ctrl_map["j_arm1_8"] = XBot::ControlMode::Idle();
+    ctrl_map["j_arm2_8"] = XBot::ControlMode::Idle();
+  
+    robot->setControlMode(ctrl_map);
+    
+    Eigen::VectorXd  K_end(7);
+    robot->arm(0).getStiffness(K_0);
+    K_end = K_0;
+    K_end.head(4).setZero();
+    
+    const int N_ITER = 200;
+    for(int k = 0; k < N_ITER; k++)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            Eigen::VectorXd arm_k(robot->arm(i).getJointNum());
+            arm_k = K_0 + k/(N_ITER-1)*(K_end-K_0);
+            robot->arm(i).setStiffness(arm_k);
+        }
+        
+        robot->move();
+        
+        ros::Duration(0.01).sleep();
+    }
+   
+}
+
+void set_arms_high_stiffness(XBot::RobotInterface::Ptr robot, const Eigen::VectorXd& K_0)
+{
+    
+    return;
+       
+    robot->setControlMode(XBot::ControlMode::Stiffness() + XBot::ControlMode::Damping());
+    
+    // dont send any impedance to the wheels
+    std::map<std::string, XBot::ControlMode> ctrl_map;
+    ctrl_map["j_arm1_8"] = XBot::ControlMode::Idle();
+    ctrl_map["j_arm2_8"] = XBot::ControlMode::Idle();
+  
+    robot->setControlMode(ctrl_map);
+    
+    Eigen::VectorXd  K;
+    robot->arm(0).getStiffness(K);
+ 
+    const int N_ITER = 200;
+    for(int k = 0; k < N_ITER; k++)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            Eigen::VectorXd arm_k(robot->arm(i).getJointNum());
+            arm_k = K + k/(N_ITER-1)*(K_0-K);
+            robot->arm(i).setStiffness(arm_k);
         }
         
         robot->move();
@@ -525,7 +593,7 @@ int main(int argc, char **argv)
     
     g_fmap_ptr = &f_est_map;
     
-    set_low_stiffness(robot);
+    set_legs_low_stiffness(robot);
    
     for (int i : {0,1,2,3})  
     {
@@ -692,9 +760,34 @@ int main(int argc, char **argv)
     Eigen::Vector3d f_manip = ext_w.head(3)/2.0;
     ext_w.tail(3) = (left_arm - com_ref).cross(f_manip) + (right_arm - com_ref).cross(f_manip);
     
+    Eigen::VectorXd K_0(7);
+    
     fpub.send_wrench_manip(ext_w); 
     
-
+    set_arms_low_stiffness(robot, K_0);
+    
+    
+    double arm_disp = 0.05;
+    bool arm_stop_push = false;
+    
+    ci.getPoseFromTf("arm1_8", "ci/pelvis", pose);
+    Eigen::Vector3d start_pos = pose.translation();
+    
+    while (!arm_stop_push) 
+    {
+        ros::spinOnce();
+	
+	model->syncFrom(*robot, XBot::Sync::All, XBot::Sync::MotorSide);
+	model->getPose("arm1_8", "pelvis", pose);
+	
+	if( (start_pos - pose.translation()).norm() >= arm_disp )
+	    arm_stop_push = true;
+    }
+       
+    set_arms_high_stiffness(robot, K_0);
+    
+    fpub.send_wrench_manip(Eigen::Vector6d::Zero()); 
+    
     while (ros::ok()) 
     {
         ros::spinOnce();
